b2b929a8207340134a6b1e3822661c2821792b59
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2023-09-16 10:32:26 +0100

    Test with one more layer of choices

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,5 @@
 # choosethis 0.0.0.9000
 
 * Set up package and documentation.
-* Included demo functions `begin()`, `left()` and `right()`.
+* Included demo functions to start an adventure, make another choice and reach an end state.
 * Simplified `choosethis.show_links` option.
--- R/choose.R
+++ R/choose.R
@@ -34,22 +34,89 @@ begin <- function(
 
 #' Go Left
 #'
+#' @param show_links Logical. Present clickable links in the R console?
+#'     Defaults to the result of [cli::ansi_has_hyperlink_support].
+#'
+#' @details Clickable ANSI links are only possible in certain terminals,
+#'     including RStudio's console but not macOS's Terminal. Use
+#'     `options(choosethis.show_links = FALSE)` to force text prompts rather
+#'     than clickable links.
+#'
 #' @return Nothing
 #'
 #' @examples left()
 #'
 #' @export
-left <- function() {
-  cli::cli_text("You win!")
+left <- function(
+    show_links = getOption("choosethis.show_links", cli::ansi_has_hyperlink_support())
+) {
+
+  cli::cli_text("There's a monster!")
+
+  if (show_links) {
+    cli::cli_text("* {.run [Fight](choosethis::fight())}")
+    cli::cli_text("* {.run [Befriend](choosethis::befriend())}")
+  }
+
+  if (!show_links) {
+    cli::cli_text("* Fight [choosethis::fight()]")
+    cli::cli_text("* Befriend [choosethis::befriend()]")
+    cli::cli_text("Copy-paste the code for your option into the console.")
+  }
+
+}
+
+#' Fight
+#'
+#' @return Nothing
+#'
+#' @examples fight()
+#'
+#' @export
+fight <- function() {
+  cli::cli_text("You died.")
+}
+
+#' Befriend
+#'
+#' @return Nothing
+#'
+#' @examples befriend()
+#'
+#' @export
+befriend <- function() {
+  cli::cli_text("You made the monster happy. You win!")
 }
 
 #' Go Right
+#'
+#' @param show_links Logical. Present clickable links in the R console?
+#'     Defaults to the result of [cli::ansi_has_hyperlink_support].
+#'
+#' @details Clickable ANSI links are only possible in certain terminals,
+#'     including RStudio's console but not macOS's Terminal. Use
+#'     `options(choosethis.show_links = FALSE)` to force text prompts rather
+#'     than clickable links.
 #'
 #' @return Nothing
 #'
 #' @examples right()
 #'
 #' @export
-right <- function() {
-  cli::cli_text("You died.")
+right <- function(
+    show_links = getOption("choosethis.show_links", cli::ansi_has_hyperlink_support())
+) {
+
+  cli::cli_text("There's a monster!")
+
+  if (show_links) {
+    cli::cli_text("* {.run [Fight](choosethis::fight())}")
+    cli::cli_text("* {.run [Befriend](choosethis::befriend())}")
+  }
+
+  if (!show_links) {
+    cli::cli_text("* Fight [choosethis::fight()]")
+    cli::cli_text("* Befriend [choosethis::befriend()]")
+    cli::cli_text("Copy-paste the code for your option into the console.")
+  }
 }
